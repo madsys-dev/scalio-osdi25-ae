14ed79094aceffaa069c48615c3fe02e18683e8f
UNIDY2002 <UNIDY2002@outlook.com>
2024-06-04 17:28:03 +0800

    Fix compilation error

--- spdk/app/leed/kv_bucket.cpp
+++ spdk/app/leed/kv_bucket.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <cstdint>
+#include <cstdio>
 #include <list>
 #include <map>
 #include <unordered_map>
--- spdk/app/leed/kv_etcd/main.go
+++ spdk/app/leed/kv_etcd/main.go
@@ -14,7 +14,7 @@ import (
 	"time"
 	"unsafe"
 
-	"go.etcd.io/etcd/api/v3/mvccpb"
+	"go.etcd.io/etcd/api/mvccpb"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
